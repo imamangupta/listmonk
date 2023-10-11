e975582a61eb5682258051d383423405a1cf6885
Aman Gupta <amanwebdevv@gmail.com>
2023-10-11 21:33:49 +0530

    Updated Home Page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    
+    <div style={{margin:'30px', textAlign:"center"}}>
+      <Link to="/admin">
+        <button className='list_num_btn'>Go to Dashboard</button>
+      </Link>
+    </div>
+    </>
   )
 }
